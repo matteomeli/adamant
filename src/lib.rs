59fc0efa73b1b5d969d378c067cd7b628854dba5
Matteo Meli <matteo.meli@gmail.com>
2019-08-06 23:37:14 +0100

    Add window and remaining initialization

--- src/lib.rs
+++ src/lib.rs
@@ -4,10 +4,13 @@ pub use com::ComPtr;
 
 use bitflags::bitflags;
 
-use log::{error, info, trace, warn};
+use log::{info, trace, warn};
 
-use winapi::shared::winerror::{FAILED, SUCCEEDED};
-use winapi::shared::{dxgi, dxgi1_3, dxgi1_4, dxgi1_5, dxgi1_6, minwindef};
+use winapi::shared::{
+    dxgi, dxgi1_2, dxgi1_3, dxgi1_4, dxgi1_5, dxgi1_6, dxgiformat, dxgitype, minwindef,
+    windef::HWND,
+    winerror::{FAILED, SUCCEEDED},
+};
 use winapi::um::{d3d12, d3d12sdklayers, d3dcommon, dxgidebug, synchapi, winnt};
 use winapi::Interface;
 
@@ -23,7 +26,35 @@ bitflags! {
     }
 }
 
-pub fn init_d3d12(flags: InitFlags) {
+pub struct InitParams {
+    pub window_handle: HWND,
+    pub window_width: u32,
+    pub window_height: u32,
+    pub back_buffer_format: dxgiformat::DXGI_FORMAT,
+    pub depth_buffer_format: dxgiformat::DXGI_FORMAT,
+    pub back_buffer_count: u32,
+    pub min_feature_level: d3dcommon::D3D_FEATURE_LEVEL,
+    pub flags: InitFlags,
+}
+
+impl InitParams {
+    pub fn new(window_handle: HWND, window_width: u32, window_height: u32) -> Self {
+        Self {
+            window_handle,
+            window_width,
+            window_height,
+            back_buffer_format: dxgiformat::DXGI_FORMAT_B8G8R8A8_UNORM,
+            depth_buffer_format: dxgiformat::DXGI_FORMAT_D32_FLOAT,
+            back_buffer_count: 2,
+            min_feature_level: d3dcommon::D3D_FEATURE_LEVEL_11_0,
+            flags: InitFlags::empty(),
+        }
+    }
+}
+
+pub fn init_d3d12(params: InitParams) {
+    trace!("Initializing D3D12 layer");
+
     // Enable debug layer
     let factory_flags = enable_debug_layer();
 
@@ -31,8 +62,8 @@ pub fn init_d3d12(flags: InitFlags) {
     let factory = create_factory(factory_flags);
 
     // Determine if tearing is supported for fullscreen borderless windows
-    let mut _allow_tearing = false;
-    if flags.contains(InitFlags::ALLOW_TEARING) {
+    let mut is_tearing_allowed = false;
+    if params.flags.contains(InitFlags::ALLOW_TEARING) {
         trace!("Checking variable refresh rate display support");
         unsafe {
             if let Ok(factory5) = factory.cast::<dxgi1_5::IDXGIFactory5>() {
@@ -43,24 +74,22 @@ pub fn init_d3d12(flags: InitFlags) {
                     mem::size_of::<minwindef::BOOL>() as _,
                 );
                 if SUCCEEDED(hr) && allow_tearing_feature == minwindef::TRUE {
-                    _allow_tearing = true;
+                    is_tearing_allowed = true;
                 }
             }
         }
-        if _allow_tearing {
+        if is_tearing_allowed {
             info!("Variable refresh rate displays supported");
         } else {
             warn!("Variable refresh rate displays not supported");
         }
     }
 
-    let min_feature_level = d3dcommon::D3D_FEATURE_LEVEL_11_0;
-
     // Get adapter
-    let adapter = get_adapter(&factory, min_feature_level);
+    let adapter = get_adapter(&factory, params.min_feature_level);
 
     // Create D3D12 API device
-    let device = create_device(&adapter, min_feature_level);
+    let device = create_device(&adapter, params.min_feature_level);
 
     // Configure debug device
     #[cfg(debug_assertions)]
@@ -115,24 +144,21 @@ pub fn init_d3d12(flags: InitFlags) {
         )) {
             feature_levels.MaxSupportedFeatureLevel
         } else {
-            min_feature_level
+            params.min_feature_level
         }
     };
 
     // Create command queue
-    let _command_queue = create_command_queue(&device);
-
-    // The number of back buffers in the swap chain
-    let back_buffer_count = 2;
+    let command_queue = create_command_queue(&device);
 
     // Create descriptor heaps for render target and depth stencil views
-    let _rtv_descriptor_heap = create_rtv_descriptor_heap(&device, back_buffer_count);
-    let _rtv_descriptor_size =
+    let rtv_descriptor_heap = create_rtv_descriptor_heap(&device, params.back_buffer_count);
+    let rtv_descriptor_size =
         unsafe { device.GetDescriptorHandleIncrementSize(d3d12::D3D12_DESCRIPTOR_HEAP_TYPE_RTV) };
-    let _dsv_descriptor_heap = create_dsv_descriptor_heap(&device);
+    let dsv_descriptor_heap = create_dsv_descriptor_heap(&device);
 
     // Create a command allocator for each back buffer that will be rendered to
-    let command_allocators = create_command_allocators(&device, back_buffer_count);
+    let command_allocators = create_command_allocators(&device, params.back_buffer_count);
 
     // Create a command list for recording graphics commands
     let _command_list = create_command_list(&device, &command_allocators[0]);
@@ -142,6 +168,69 @@ pub fn init_d3d12(flags: InitFlags) {
     let mut _back_buffer_index = 0;
     let fence_values: [u64; MAX_BACK_BUFFER_COUNT] = [0, 0, 0];
     let (_fence, _fence_event) = create_fence(&device, fence_values[0]);
+
+    // Compute appropriate back buffer format
+    let back_buffer_format = no_srgb(params.back_buffer_format);
+
+    // TODO: When updating window size instead
+    // 0) Wait for GPU to finish work
+    // 1) Release render targets (and depth stencil) and reset fence values
+    // 2) Resize swap chain
+    // 3) Recreate all below
+
+    // Create swapchain
+    let swap_chain = create_swap_chain(
+        &factory,
+        &command_queue,
+        params.window_handle,
+        params.window_width,
+        params.window_height,
+        back_buffer_format,
+        params.back_buffer_count,
+        is_tearing_allowed,
+    );
+
+    // Handle HDR output
+    let _color_space = update_color_space(&swap_chain, back_buffer_format, params.flags);
+
+    // Create render targets for each bak buffer
+    let _render_targets = create_render_targets(
+        &device,
+        &swap_chain,
+        &rtv_descriptor_heap,
+        back_buffer_format,
+        params.back_buffer_count,
+        rtv_descriptor_size,
+    );
+
+    let _back_buffer_index = unsafe { swap_chain.GetCurrentBackBufferIndex() };
+
+    // Allocate a 2-D surface as the depth/stencil buffer and create a depth/stencil view on this surface
+    let _depth_stencil = create_depth_stencil(
+        &device,
+        &dsv_descriptor_heap,
+        params.depth_buffer_format,
+        params.window_width,
+        params.window_height,
+    );
+
+    // Set rendering viewport and scissor rectangle to fit client window
+    let _viewport = d3d12::D3D12_VIEWPORT {
+        TopLeftX: 0.0,
+        TopLeftY: 0.0,
+        Width: params.window_width as _,
+        Height: params.window_height as _,
+        MinDepth: d3d12::D3D12_MIN_DEPTH,
+        MaxDepth: d3d12::D3D12_MAX_DEPTH,
+    };
+    let _scissor_rect = d3d12::D3D12_RECT {
+        left: 0,
+        top: 0,
+        right: params.window_width as _,
+        bottom: params.window_height as _,
+    };
+
+    info!("D3D12 layer initialized successfully");
 }
 
 fn enable_debug_layer() -> u32 {
@@ -241,8 +330,7 @@ fn get_adapter(
                     let mut desc = dxgi::DXGI_ADAPTER_DESC1 { ..mem::zeroed() };
                     let hr = adapter.GetDesc1(&mut desc);
                     if FAILED(hr) {
-                        error!("Failed to get adapter description");
-                        panic!();
+                        panic!("Failed to get adapter description");
                     }
 
                     // Skip the Basic Render Driver adapter.
@@ -273,8 +361,7 @@ fn get_adapter(
                 let mut desc = dxgi::DXGI_ADAPTER_DESC1 { ..mem::zeroed() };
                 let hr = current_adapter.GetDesc1(&mut desc);
                 if FAILED(hr) {
-                    error!("Failed to get adapter description");
-                    panic!();
+                    panic!("Failed to get adapter description");
                 }
 
                 // Skip the Basic Render Driver adapter.
@@ -473,7 +560,7 @@ fn create_command_allocators(
             )) {
                 info!("D3D12 command allocator created for back buffer {}", n);
                 command_allocator.SetName(
-                    format!("AdamantRenderTarget{}", n)
+                    format!("AdamantCommandAllocator{}", n)
                         .encode_utf16()
                         .collect::<Vec<u16>>()
                         .as_ptr(),
@@ -553,4 +640,254 @@ fn create_fence(
         )
     };
     (fence, fence_event)
+}
+
+fn no_srgb(format: dxgiformat::DXGI_FORMAT) -> dxgiformat::DXGI_FORMAT {
+    match format {
+        dxgiformat::DXGI_FORMAT_R8G8B8A8_UNORM_SRGB => dxgiformat::DXGI_FORMAT_R8G8B8A8_UNORM,
+        dxgiformat::DXGI_FORMAT_B8G8R8A8_UNORM_SRGB => dxgiformat::DXGI_FORMAT_B8G8R8A8_UNORM,
+        dxgiformat::DXGI_FORMAT_B8G8R8X8_UNORM_SRGB => dxgiformat::DXGI_FORMAT_B8G8R8X8_UNORM,
+        _ => format,
+    }
+}
+
+fn create_swap_chain(
+    factory: &ComPtr<dxgi1_4::IDXGIFactory4>,
+    command_queue: &ComPtr<d3d12::ID3D12CommandQueue>,
+    window_handle: HWND,
+    window_width: u32,
+    window_height: u32,
+    back_buffer_format: dxgiformat::DXGI_FORMAT,
+    back_buffer_count: u32,
+    is_tearing_allowed: bool,
+) -> ComPtr<dxgi1_4::IDXGISwapChain3> {
+    trace!("Creating D3D12 swap chain");
+    unsafe {
+        let desc = dxgi1_2::DXGI_SWAP_CHAIN_DESC1 {
+            Width: window_width,
+            Height: window_height,
+            Format: back_buffer_format,
+            SampleDesc: dxgitype::DXGI_SAMPLE_DESC {
+                Count: 1,
+                Quality: 0,
+            },
+            BufferUsage: dxgitype::DXGI_USAGE_RENDER_TARGET_OUTPUT,
+            BufferCount: back_buffer_count,
+            Scaling: dxgi1_2::DXGI_SCALING_STRETCH,
+            SwapEffect: dxgi::DXGI_SWAP_EFFECT_FLIP_DISCARD,
+            AlphaMode: dxgi1_2::DXGI_ALPHA_MODE_IGNORE,
+            Flags: if is_tearing_allowed {
+                dxgi::DXGI_SWAP_CHAIN_FLAG_ALLOW_TEARING
+            } else {
+                0
+            },
+            ..mem::zeroed()
+        };
+        let fullscreen_desc = dxgi1_2::DXGI_SWAP_CHAIN_FULLSCREEN_DESC {
+            Windowed: minwindef::TRUE,
+            ..mem::zeroed()
+        };
+        let mut swap_chain = ComPtr::<dxgi1_2::IDXGISwapChain1>::null();
+        if SUCCEEDED(factory.CreateSwapChainForHwnd(
+            command_queue.as_raw() as *mut _,
+            window_handle,
+            &desc,
+            &fullscreen_desc,
+            ptr::null_mut(),
+            swap_chain.as_mut_void() as *mut *mut _,
+        )) {
+            info!("D3D12 swapchain created");
+        } else {
+            panic!("Failed to create D3D12 swapchain");
+        }
+        if let Ok(swap_chain3) = swap_chain.cast::<dxgi1_4::IDXGISwapChain3>() {
+            // Does not support exclusive full-screen mode and prevents DXGI from responding to the ALT+ENTER shortcut
+            let hr = factory.MakeWindowAssociation(window_handle, 1 << 1); // DXGI_MWA_NO_ALT_ENTER (can't find it in winit)
+            if FAILED(hr) {
+                panic!("Failed to disable ALT+ENTER shortcut to go fullscreen");
+            }
+            swap_chain3
+        } else {
+            panic!("Failed to create D3D12 swapchain")
+        }
+    }
+}
+
+fn update_color_space(
+    swap_chain: &ComPtr<dxgi1_4::IDXGISwapChain3>,
+    back_buffer_format: dxgiformat::DXGI_FORMAT,
+    flags: InitFlags,
+) -> dxgitype::DXGI_COLOR_SPACE_TYPE {
+    let mut is_hdr10_supported = false;
+    let mut output = ComPtr::<dxgi::IDXGIOutput>::null();
+    unsafe {
+        if SUCCEEDED(swap_chain.GetContainingOutput(output.as_mut_void() as *mut *mut _)) {
+            if let Ok(output6) = output.cast::<dxgi1_6::IDXGIOutput6>() {
+                let mut desc = dxgi1_6::DXGI_OUTPUT_DESC1 { ..mem::zeroed() };
+                if FAILED(output6.GetDesc1(&mut desc)) {
+                    panic!("Failed to retrieve DXGI output description");
+                }
+
+                if desc.ColorSpace == dxgitype::DXGI_COLOR_SPACE_RGB_FULL_G2084_NONE_P2020 {
+                    is_hdr10_supported = true;
+                }
+            }
+        }
+    }
+
+    let color_space = if flags.contains(InitFlags::ENABLE_HDR) && is_hdr10_supported {
+        match back_buffer_format {
+            // The application creates the HDR10 signal.
+            dxgiformat::DXGI_FORMAT_R10G10B10A2_UNORM => {
+                dxgitype::DXGI_COLOR_SPACE_RGB_FULL_G2084_NONE_P2020
+            }
+            // The system creates the HDR10 signal; application uses linear values.
+            dxgiformat::DXGI_FORMAT_R16G16B16A16_FLOAT => {
+                dxgitype::DXGI_COLOR_SPACE_RGB_FULL_G10_NONE_P709
+            }
+            _ => dxgitype::DXGI_COLOR_SPACE_RGB_FULL_G22_NONE_P709,
+        }
+    } else {
+        dxgitype::DXGI_COLOR_SPACE_RGB_FULL_G22_NONE_P709
+    };
+
+    let mut color_space_support = 0;
+    unsafe {
+        if SUCCEEDED(swap_chain.CheckColorSpaceSupport(color_space, &mut color_space_support))
+            && (color_space_support & dxgi1_4::DXGI_SWAP_CHAIN_COLOR_SPACE_SUPPORT_FLAG_PRESENT)
+                != 0
+            && FAILED(swap_chain.SetColorSpace1(color_space))
+        {
+            panic!("Failed to set swapchain's color space to support HDR");
+        }
+    }
+    color_space
+}
+
+fn create_render_targets(
+    device: &ComPtr<d3d12::ID3D12Device>,
+    swap_chain: &ComPtr<dxgi1_4::IDXGISwapChain3>,
+    rtv_descriptor_heap: &ComPtr<d3d12::ID3D12DescriptorHeap>,
+    back_buffer_format: dxgiformat::DXGI_FORMAT,
+    back_buffer_count: u32,
+    rtv_descriptor_size: u32,
+) -> Vec<ComPtr<d3d12::ID3D12Resource>> {
+    trace!(
+        "Creating D3D12 render target views for {} back buffers",
+        back_buffer_count
+    );
+    let mut render_targets = Vec::with_capacity(back_buffer_count as _);
+    unsafe {
+        for n in 0..back_buffer_count {
+            let mut render_target = ComPtr::<d3d12::ID3D12Resource>::null();
+            if SUCCEEDED(swap_chain.GetBuffer(
+                n,
+                &d3d12::ID3D12Resource::uuidof(),
+                render_target.as_mut_void(),
+            )) {
+                info!("D3D12 render target view created for back buffer {}", n);
+                render_target.SetName(
+                    format!("AdamantRenderTarget{}", n)
+                        .encode_utf16()
+                        .collect::<Vec<u16>>()
+                        .as_ptr(),
+                );
+            } else {
+                panic!(
+                    "Failed to create D3D12 render target view for back buffer {}",
+                    n
+                );
+            }
+
+            let rtv_desc = d3d12::D3D12_RENDER_TARGET_VIEW_DESC {
+                Format: back_buffer_format,
+                ViewDimension: d3d12::D3D12_RTV_DIMENSION_TEXTURE2D,
+                ..mem::zeroed()
+            };
+            let rtv_descriptor_handle = d3d12::D3D12_CPU_DESCRIPTOR_HANDLE {
+                ptr: rtv_descriptor_heap.GetCPUDescriptorHandleForHeapStart().ptr
+                    + (rtv_descriptor_size * n) as usize,
+            };
+            device.CreateRenderTargetView(render_target.as_raw(), &rtv_desc, rtv_descriptor_handle);
+
+            render_targets.push(render_target);
+        }
+    }
+    render_targets
+}
+
+fn create_depth_stencil(
+    device: &ComPtr<d3d12::ID3D12Device>,
+    dsv_descriptor_heap: &ComPtr<d3d12::ID3D12DescriptorHeap>,
+    depth_buffer_format: dxgiformat::DXGI_FORMAT,
+    window_width: u32,
+    window_height: u32,
+) -> ComPtr<d3d12::ID3D12Resource> {
+    trace!("Creating D3D12 swap chain");
+    let depth_heap_properties = d3d12::D3D12_HEAP_PROPERTIES {
+        Type: d3d12::D3D12_HEAP_TYPE_DEFAULT,
+        CPUPageProperty: d3d12::D3D12_CPU_PAGE_PROPERTY_UNKNOWN,
+        MemoryPoolPreference: d3d12::D3D12_MEMORY_POOL_UNKNOWN,
+        CreationNodeMask: 1,
+        VisibleNodeMask: 1,
+    };
+    let depth_stencil_desc = d3d12::D3D12_RESOURCE_DESC {
+        Dimension: d3d12::D3D12_RESOURCE_DIMENSION_TEXTURE2D,
+        Alignment: 0,
+        Width: u64::from(window_width),
+        Height: window_height,
+        DepthOrArraySize: 1,
+        MipLevels: 1,
+        Format: depth_buffer_format,
+        SampleDesc: dxgitype::DXGI_SAMPLE_DESC {
+            Count: 1,
+            Quality: 0,
+        },
+        Layout: d3d12::D3D12_TEXTURE_LAYOUT_UNKNOWN,
+        Flags: d3d12::D3D12_RESOURCE_FLAG_ALLOW_DEPTH_STENCIL,
+    };
+
+    let mut depth_stencil = ComPtr::<d3d12::ID3D12Resource>::null();
+    unsafe {
+        let mut depth_optimized_clear_value = d3d12::D3D12_CLEAR_VALUE {
+            Format: depth_buffer_format,
+            ..mem::zeroed()
+        };
+        *depth_optimized_clear_value.u.DepthStencil_mut() = d3d12::D3D12_DEPTH_STENCIL_VALUE {
+            Depth: 1.0,
+            Stencil: 0,
+        };
+        if SUCCEEDED(device.CreateCommittedResource(
+            &depth_heap_properties,
+            d3d12::D3D12_HEAP_FLAG_NONE,
+            &depth_stencil_desc,
+            d3d12::D3D12_RESOURCE_STATE_DEPTH_WRITE,
+            &depth_optimized_clear_value,
+            &d3d12::ID3D12Resource::uuidof(),
+            depth_stencil.as_mut_void(),
+        )) {
+            info!("D3D12 depth/stencil resource created");
+        } else {
+            panic!("Failed to create D3D12 depth/stencil resource");
+        }
+
+        depth_stencil.SetName(
+            "AdamantDepthStencil"
+                .encode_utf16()
+                .collect::<Vec<u16>>()
+                .as_ptr(),
+        );
+
+        let dsv_desc = d3d12::D3D12_DEPTH_STENCIL_VIEW_DESC {
+            Format: depth_buffer_format,
+            ViewDimension: d3d12::D3D12_DSV_DIMENSION_TEXTURE2D,
+            ..mem::zeroed()
+        };
+        device.CreateDepthStencilView(
+            depth_stencil.as_raw(),
+            &dsv_desc,
+            dsv_descriptor_heap.GetCPUDescriptorHandleForHeapStart(),
+        );
+    }
+    depth_stencil
 }
--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,18 @@ use adamant;
 
 use env_logger::{self, Env};
 
+use log::info;
+
+use winit::{
+    dpi::LogicalSize,
+    event::{Event, WindowEvent},
+    event_loop::{ControlFlow, EventLoop},
+    window::WindowBuilder,
+};
+
+#[cfg(target_os = "windows")]
+use winit::platform::windows::WindowExtWindows;
+
 fn main() {
     let env = Env::default()
         .filter_or("MY_LOG_LEVEL", "trace")
@@ -9,6 +21,51 @@ fn main() {
 
     env_logger::init_from_env(env);
 
-    let init_flags = adamant::InitFlags::ALLOW_TEARING;
-    adamant::init_d3d12(init_flags);
+    const WINDOW_WIDTH: u32 = 1280;
+    const WINDOW_HEIGHT: u32 = 720;
+    let event_loop = EventLoop::new();
+    let window = WindowBuilder::new()
+        .with_inner_size(LogicalSize::new(
+            f64::from(WINDOW_WIDTH),
+            f64::from(WINDOW_HEIGHT),
+        ))
+        .with_title("Adamant Window")
+        .build(&event_loop)
+        .unwrap();
+
+    let mut init_params = adamant::InitParams::new(window.hwnd() as *mut _, 1280, 720);
+    init_params.flags = adamant::InitFlags::ALLOW_TEARING | adamant::InitFlags::ENABLE_HDR;
+    adamant::init_d3d12(init_params);
+
+    event_loop.run(move |event, _, control_flow| {
+        match event {
+            Event::EventsCleared => {
+                window.request_redraw();
+            }
+            Event::WindowEvent {
+                event: WindowEvent::RedrawRequested,
+                ..
+            } => {
+                // Redraw the application.
+                //
+                // It's preferrable to render in this event rather than in EventsCleared, since
+                // rendering in here allows the program to gracefully handle redraws requested
+                // by the OS.
+            }
+            Event::WindowEvent {
+                event: WindowEvent::CloseRequested,
+                ..
+            } => {
+                info!("The close button was pressed; stopping");
+                *control_flow = ControlFlow::Exit
+            }
+            // ControlFlow::Poll continuously runs the event loop, even if the OS hasn't
+            // dispatched any events. This is ideal for games and similar applications.
+            _ => *control_flow = ControlFlow::Poll,
+            // ControlFlow::Wait pauses the event loop if no events are available to process.
+            // This is ideal for non-game applications that only update in response to user
+            // input, and uses significantly less power/CPU time than ControlFlow::Poll.
+            // _ => *control_flow = ControlFlow::Wait,
+        }
+    });
 }
